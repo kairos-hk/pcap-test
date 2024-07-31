a2d8456a28fc3265299c9b4abbc44090bb4d54ef
DongyoungKim <kairos.hk0912@gmail.com>
2024-07-31 16:02:17 +0900

    oh... struct...

--- pcap-test.c
+++ pcap-test.c
@@ -1,61 +1,83 @@
-#include <netinet/if_ether.h>
-#include <netinet/ip.h>
-#include <netinet/tcp.h>
-#include <arpa/inet.h>
-#include <stdio.h>
 #include <pcap.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <libnet.h>
+#include <arpa/inet.h>
 
-void print_ethernet_header(const u_char *packet) {
-    struct ether_header *eth_header = (struct ether_header *)packet;
+void usage() {
+    printf("./pcap-test <interface>\n");
+}
+
+typedef struct {
+    char* dev;
+} Param;
+
+bool parse(Param* param, int argc, char* argv[]) {
+    if (argc != 2) {
+        usage();
+        return false;
+    }
+    param->dev = argv[1];
+    return true;
+}
+
+typedef struct {
+    struct libnet_ethernet_hdr* eth_header;
+    struct libnet_ipv4_hdr* ip_header;
+    struct libnet_tcp_hdr* tcp_header;
+    const u_char* payload;
+    int payload_len;
+} PacketInfo;
+
+void parse_packet(const u_char* packet, int packet_len, PacketInfo* info) {
+    info->eth_header = (struct libnet_ethernet_hdr*) packet;
+    info->ip_header = (struct libnet_ipv4_hdr*) (packet + sizeof(struct libnet_ethernet_hdr));
+    info->tcp_header = (struct libnet_tcp_hdr*) ((u_char*) info->ip_header + (info->ip_header->ip_hl << 2));
+    info->payload = (u_char*) info->tcp_header + (info->tcp_header->th_off << 2);
+    info->payload_len = packet_len - (info->payload - packet);
+}
+
+void print_packet_info(const PacketInfo* info) {
     printf("SRC MAC: %02x:%02x:%02x:%02x:%02x:%02x\n", 
-           eth_header->ether_shost[0], eth_header->ether_shost[1], eth_header->ether_shost[2],
-           eth_header->ether_shost[3], eth_header->ether_shost[4], eth_header->ether_shost[5]);
+           info->eth_header->ether_shost[0], info->eth_header->ether_shost[1], info->eth_header->ether_shost[2],
+           info->eth_header->ether_shost[3], info->eth_header->ether_shost[4], info->eth_header->ether_shost[5]);
     printf("DST MAC: %02x:%02x:%02x:%02x:%02x:%02x\n", 
-           eth_header->ether_dhost[0], eth_header->ether_dhost[1], eth_header->ether_dhost[2],
-           eth_header->ether_dhost[3], eth_header->ether_dhost[4], eth_header->ether_dhost[5]);
-}
+           info->eth_header->ether_dhost[0], info->eth_header->ether_dhost[1], info->eth_header->ether_dhost[2],
+           info->eth_header->ether_dhost[3], info->eth_header->ether_dhost[4], info->eth_header->ether_dhost[5]);
 
-void print_ip_header(const u_char *packet) {
-    struct ip *ip_header = (struct ip *)(packet + sizeof(struct ether_header));
-    printf("SRC IP: %s\n", inet_ntoa(ip_header->ip_src));
-    printf("DST IP: %s\n", inet_ntoa(ip_header->ip_dst));
-}
+    printf("SRC IP: %s\n", inet_ntoa(info->ip_header->ip_src));
+    printf("DST IP: %s\n", inet_ntoa(info->ip_header->ip_dst));
 
-void print_tcp_header(const u_char *packet) {
-    struct ip *ip_header = (struct ip *)(packet + sizeof(struct ether_header));
-    struct tcphdr *tcp_header = (struct tcphdr *)((u_char *)ip_header + (ip_header->ip_hl << 2));
-    printf("SRC PORT: %d\n", ntohs(tcp_header->th_sport));
-    printf("DST PORT: %d\n", ntohs(tcp_header->th_dport));
-}
-
-void print_payload(const u_char *packet, int packet_len) {
-    struct ip *ip_header = (struct ip *)(packet + sizeof(struct ether_header));
-    struct tcphdr *tcp_header = (struct tcphdr *)((u_char *)ip_header + (ip_header->ip_hl << 2));
-    u_char *data = (u_char *)tcp_header + (tcp_header->th_off << 2);
-    int data_len = packet_len - (data - packet);
+    printf("SRC PORT: %d\n", ntohs(info->tcp_header->th_sport));
+    printf("DST PORT: %d\n", ntohs(info->tcp_header->th_dport));
 
     printf("DATA: ");
-    for (int i = 0; i < data_len && i < 20; i++) {
-        printf("%02x ", data[i]);
+    for (int i = 0; i < info->payload_len && i < 20; i++) {
+        printf("%02x ", info->payload[i]);
     }
     printf("\n");
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
+    Param param;
+    if (!parse(&param, argc, argv))
+        return -1;
+
     char errbuf[PCAP_ERRBUF_SIZE];
-    pcap_t *handle = pcap_open_live(argv[1], BUFSIZ, 1, 1000, errbuf);
+    pcap_t* pcap = pcap_open_live(param.dev, BUFSIZ, 1, 1000, errbuf);
 
-    struct pcap_pkthdr header;
-    const u_char *packet;
+    while (true) {
+        struct pcap_pkthdr* header;
+        const u_char* packet;
+        int res = pcap_next_ex(pcap, &header, &packet);
+        if (res <= 0) continue;
 
-    while ((packet = pcap_next(handle, &header))) {
-        print_ethernet_header(packet);
-        print_ip_header(packet);
-        print_tcp_header(packet);
-        print_payload(packet, header.len);
+        PacketInfo info;
+        parse_packet(packet, header->caplen, &info);
+
+        print_packet_info(&info);
         printf("\n");
     }
 
-    pcap_close(handle);
-    return 0;
+    pcap_close(pcap);
 }
